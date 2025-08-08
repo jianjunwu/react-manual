f7a82064b7884d8b1e0a7e0137becf56ed358ea1
吴建军 <nicholas_wu@aliyun.com>
2025-08-08 16:31:04 +0800

    更新后端

--- MERN-CRASH-COURSE/backend/server.js
+++ MERN-CRASH-COURSE/backend/server.js
@@ -1,12 +1,22 @@
 // const express = require('express');
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+// import Product from "./models/product.model.js";
+
+import router from "./routes/product.route.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World! nic');
-});
+app.use(express.json()); //allows us to parse json
 
-app.listen(5000, () =>
-  console.log('Server started at http://localhost:5000')
-);
+app.use("/api/products", router);
+
+
+
+app.listen(5000, () =>{
+    connectDB();
+    console.log('Server started at http://localhost:5000')
+});
